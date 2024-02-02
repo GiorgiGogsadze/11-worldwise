f05b9c67f9e7193a46dc58ed0fc4ae5a4429fb87
GiorgiGogsadze <gogsadzegeorge42@gmail.com>
2024-02-02 08:16:38 +0400

    start developing single page application

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,15 @@
+import styles from "./Sidebar.module.css";
+import Logo from "./Logo";
+import AppNav from "./AppNav";
+export default function Sidebar() {
+  return (
+    <div className={styles.sidebar}>
+      <Logo />
+      <AppNav />
+      <p>List of cities:</p>
+      <footer className={styles.footer}>
+        <p>&copy; Copyright {new Date().getFullYear()} by WorldWise Inc.</p>
+      </footer>
+    </div>
+  );
+}
